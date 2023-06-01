95a26f4f66a95a6d5284c744b8adc3d3e0d17d16
AkshayKaginkar <117984792+AkshayKaginkar@users.noreply.github.com>
2023-06-01 08:07:56 +0530

    Assignment added

---
+++ Assignment_MovieApp/Assignment_MovieApp/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_MovieApp
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string LaunchDate { get; set; }
+        public decimal BoxOfficeCollection { get; set; }
+
+        public Movie(string title, string genre, string launchDate, decimal boxOfficeCollection)
+        {
+            Title = title;
+            Genre = genre;
+            LaunchDate = launchDate;
+            BoxOfficeCollection = boxOfficeCollection;
+        }
+    }
+}
